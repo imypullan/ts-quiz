f807502f66a5ff9e36aef194bb551e13dbb54db4
Imy <imypullan@gmail.com>
2021-06-23 14:01:30 +0100

    scaffolding for quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from 'react'
+//Components
+import QuestionCard from "./Components/QuestionCard"
 
-function App() {
-  return (
-    <div className="App">
-      Quiz
-    </div>
-  );
+const App = () => {
+
+    const startTrivia = async () => {
+
+    }
+
+    const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const nextQuestion = () => {
+
+    }
+
+    return (
+        <div className='App'>
+            <h1>REACT QUIZ</h1>
+            <button className='start' onClick={startTrivia}>
+                Start
+            </button>
+            <p className='score'>Score:</p>
+            <p>Loading Questions...</p>
+            <QuestionCard />
+            <button className='next' onClick={nextQuestion}>Next Question</button>
+        </div>
+    )
+
 }
+
 
 export default App;
