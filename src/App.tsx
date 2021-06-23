09ee5167e6cb80df51ebfa5d13f0754badbf51d5
Imy <imypullan@gmail.com>
2021-06-23 16:30:21 +0100

    styling for quiz box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Difficulty, fetchQuizQuestions, QuestionState} from "./API";
 import QuestionCard from "./Components/QuestionCard";
-import { GlobalStyle } from './App.styles'
+import { GlobalStyle, Wrapper } from './App.styles'
 
 export type AnswerObject = {
     question: string
@@ -75,7 +75,7 @@ const App = () => {
     return (
         <>
             <GlobalStyle />
-            <div className='App'>
+            <Wrapper>
                 <h1>REACT QUIZ</h1>
                 {gameOver || userAnswers.length == TOTAL_QUESTIONS?
                     <button className='start' onClick={startTrivia}>
@@ -99,7 +99,7 @@ const App = () => {
                         Next Question
                     </button>
                 ): null}
-            </div>
+            </Wrapper>
         </>
     )
 }
