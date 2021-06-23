0b3817ec8946233113eb8d25d873c5eb32c07852
Imy <imypullan@gmail.com>
2021-06-23 14:31:56 +0100

    importing functions and files into apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-//Components
-import QuestionCard from "./Components/QuestionCard"
+//Types
+import {Difficulty, fetchQuizQuestions} from "./API";
 
 const TOTAL_QUESTIONS = 10
 
@@ -11,6 +11,8 @@ const App = () => {
     const [userAnswers, setUserAnswers] = useState([])
     const [score, setScore] = useState(0)
     const [gameOver, setGameOver] = useState(true)
+
+    console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
 
 
@@ -34,14 +36,14 @@ const App = () => {
             </button>
             <p className='score'>Score:</p>
             <p>Loading Questions...</p>
-            <QuestionCard
-                questionNumber={number + 1}
-                totalQuestions={TOTAL_QUESTIONS}
-                question={questions[number].question}
-                answers={questions[number].answers}
-                userAnswer={userAnswers ? userAnswers[number] : undefined}
-                callback={checkAnswer}
-            />
+            {/*<QuestionCard*/}
+            {/*    questionNumber={number + 1}*/}
+            {/*    totalQuestions={TOTAL_QUESTIONS}*/}
+            {/*    question={questions[number].question}*/}
+            {/*    answers={questions[number].answers}*/}
+            {/*    userAnswer={userAnswers ? userAnswers[number] : undefined}*/}
+            {/*    callback={checkAnswer}*/}
+            {/*/>*/}
             <button className='next' onClick={nextQuestion}>Next Question</button>
         </div>
     )
