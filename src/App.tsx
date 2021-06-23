b7fbf089db32373a9ba6f48379ee861474aef564
Imy <imypullan@gmail.com>
2021-06-23 14:17:13 +0100

    props passing down to qcard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 //Components
 import QuestionCard from "./Components/QuestionCard"
 
+const TOTAL_QUESTIONS = 10
+
 const App = () => {
     const [loading, setLoading] = useState(false)
     const [questions, setQuestions] = useState([])
@@ -32,7 +34,14 @@ const App = () => {
             </button>
             <p className='score'>Score:</p>
             <p>Loading Questions...</p>
-            <QuestionCard />
+            <QuestionCard
+                questionNumber={number + 1}
+                totalQuestions={TOTAL_QUESTIONS}
+                question={questions[number].question}
+                answers={questions[number].answers}
+                userAnswer={userAnswers ? userAnswers[number] : undefined}
+                callback={checkAnswer}
+            />
             <button className='next' onClick={nextQuestion}>Next Question</button>
         </div>
     )
