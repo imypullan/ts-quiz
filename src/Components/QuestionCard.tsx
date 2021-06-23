50df139404caba0a84537595c608513232176361
Imy <imypullan@gmail.com>
2021-06-23 14:10:49 +0100

    qcard typescripted

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
-const QuestionCard = () => <div>Question Card</div>
+type Props = {
+    question: string
+    answers: string[]
+    callback: any
+    userAnswer: boolean
+    questionNumber: number
+    totalQuestions: number
+}
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions}) => (
+    <div>
+        <p className='number'>
+            Question: {questionNumber} / {totalQuestions}
+        </p>
+
+        <p dangerouslySetInnerHTML={{__html: question}}>
+            <div>
+                {answers.map (answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{__html: answer}} />
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </p>
+    </div>
+)
 
 export default QuestionCard
